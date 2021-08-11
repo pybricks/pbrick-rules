5d71ab8432d834d16753082d5b04e2d408128f64
David Lechner <david@pybricks.com>
2021-08-11 10:45:57 -0500

    release: move git tag before build
    This should catch failing to bump the version before a release.

--- release.sh
+++ release.sh
@@ -5,11 +5,12 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 
+git tag ${version}
+
 debuild -S
 debuild -- clean
 
 dput ppa:pybricks/ppa ../${source}_${version}_source.changes
 
-git tag ${version}
 git push
 git push --tags
