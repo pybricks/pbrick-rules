5b66b6b9753b55ad14df660c86d76d7f767507bc
David Lechner <david@pybricks.com>
2021-08-11 10:44:57 -0500

    release: fix pybricks/ppa

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ version=$(dpkg-parsechangelog -S Version)
 debuild -S
 debuild -- clean
 
-dput ppa:pybricks/tools ../${source}_${version}_source.changes
+dput ppa:pybricks/ppa ../${source}_${version}_source.changes
 
 git tag ${version}
 git push
